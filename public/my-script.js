d8ed4c8cab88fedeb371949b88bc6a20268487b8
nirmaldavis <nirmal.davis@gmail.com>
2015-10-09 17:27:34 +0530

    Using value, factory,  filter
    Usage of value, factory and filter

--- public/my-script.js
+++ public/my-script.js
@@ -1,15 +1,16 @@
-{
-    //"globals": { // Globals variables.
-    //        "jasmine": true,
-    //        "angular": true,
-    //        "browser": true,
-    //        "element": true,
-    //        "by":true,
-    //        "io":true,
-    //        "_":false,
-    //        "$":false
-    //    }
-}
+/*
+    "globals": { // Globals variables.
+            "jasmine": true,
+            "angular": true,
+            "browser": true,
+            "element": true,
+            "by":true,
+            "io":true,
+            "_":false,
+            "$":false
+        }
+*/
+
 
 var myApp = angular.module("myApp", []);
 
@@ -17,23 +18,89 @@ myApp.run(function ($rootScope) {
     console.log("myApp initialized with run()");
 });
 
-var Task = function (defaults) {
-    defaults = defaults || {};
-    this.name = defaults.name || '';
-    this.description = defaults.description || '';
-    this.completed = defaults.completed || false;
-    this.toggleMarkCompleted = function () {
+
+
+var taskClassFactory = function () {
+
+    console.log("In taskFactory function");
+
+    var Task = function (defaults) {
+        defaults = defaults || {};
+        this.name = defaults.name || '';
+        this.description = defaults.description || '';
+        this.completed = defaults.completed || false;
+    };
+
+    Task.prototype.toggleMarkCompleted = function () {
         this.completed = !this.completed;
-    }
+    };
+
+    return Task;
+
+}
+
+//myApp.value("Task", Task);
+
+myApp.factory("Task", taskClassFactory);
+
+myApp.value("defaultTrimLength", 20);
+
+var trimTextFilterFactory = function (defaultTrimLength) {
+
+    return function (data, trimLength) {
+        //        console.log("In trimTextFilterFactory - data : ", data);
+        //        console.log("In trimTextFilterFactory - trimLength : ", trimLength);
+        trimLength = trimLength || defaultTrimLength;
+        if (data) {
+            return data.length < trimLength ? data : data.substr(0, trimLength) + " ...";
+        } else
+            return data;
+    };
+
 };
 
-myApp.value("Task", Task);
+myApp.filter("trimText", trimTextFilterFactory);
+
+/*
+myApp.filter("completedTasksCount", function() {
+    return function(tasks) {
+        
+//        console.log("In completedTasksCount");
+//        
+//        console.log("tasks length " , tasks);
+        
+//        return tasks.length;
+        
+        return tasks.reduce( function(result, task) {
+//            console.log("In reduce - task.completed = ", task.completed );
+//            console.log("result = ", result);
+//            console.log("(task.completed === true) : " + (task.completed === true));
+            
+            return task.completed ? ++result : result ;
+        }, 0);
+        
+    };
+}); 
+*/
+
+//Using built in filter from controller, rather than from HTML DOM
+myApp.filter("completedTasksCount", function ($filter) {
+    var builtInFilter = $filter('filter');
+    return function (tasks) {
+        return builtInFilter(tasks, {
+            completed: true
+        }).length;
+    };
+});
+
 
 myApp.controller("myController", function ($scope, $http, Task) {
     console.log("In myController");
     console.log("arguments = ", arguments);
 
     $scope.author = "Nirmal Davis V";
+
+    $scope.tasks = [];
 
     /*    
     //    $scope.tasks = {
@@ -68,7 +135,6 @@ myApp.controller("myController", function ($scope, $http, Task) {
     */
 
 
-
     $scope.setCurrentTask = function (task) {
         //        console.log("Current selected task : " + task.description);
         $scope.currentTask = task;
@@ -94,7 +160,7 @@ myApp.controller("myController", function ($scope, $http, Task) {
         //        });
         */
 
-        $scope.tasks = [];
+        //        $scope.tasks = [];
 
         for (item of response.data) {
             $scope.tasks.push(new Task(item));
@@ -104,16 +170,6 @@ myApp.controller("myController", function ($scope, $http, Task) {
 
     });
 
-    $scope.searchTasks = function (task) {
-        //        console.log("Search Text is ", $scope.searchText);
-        //        for( i = 0; i < $scope.tasks; i++) {
-        //            if($scope.tasks[i].name.indexOf($scope.searchText) >= 0) {
-        //                
-        //            }
-        //        }
-
-
-    };
 
     myApp.filter("taskFilter", function () {
         return function (tasks, searchStr) {
@@ -134,5 +190,24 @@ myApp.controller("myController", function ($scope, $http, Task) {
     $scope.tasksSortOrder = {
         "type": "name"
     };
+
+    $scope.checkCurrentTask = function () {
+
+
+        /*
+              (typeof($scope.currentTask) === 'undefined') 
+                or 
+                ($scope.currentTask == undefined)
+        */
+
+        console.log("In checkCurrentTask");
+        if ($scope.currentTask == undefined) {
+            console.log("currentTask is undefined");
+            return false;
+        } else {
+            console.log("currentTask is = ", $scope.currentTask);
+            return true;
+        }
+    }
 
 });
